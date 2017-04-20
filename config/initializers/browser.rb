7eb463a6bd3233d88b2eb7bbd267b478e2ab1376
Josh W Lewis <josh.w.lewis@gmail.com>
2017-04-20 18:34:16 -0500

    I guess we need browser detection

--- config/initializers/browser.rb
+++ config/initializers/browser.rb
@@ -1,16 +1,39 @@
 if Rails.env.test?
+  def find_chrome_bin
+    potential_binaries = [
+      'chromium-browser',
+      'chromium',
+      'google-chrome-canary',
+      'google-chrome-unstable',
+      'google-chrome-beta'
+    ]
+
+    potential_paths = [
+      '/Applications/Chromium.app/Contents/MacOS/Chromium',
+      "#{ENV['HOME']}/Applications/Chromium.app/Contents/MacOS/Chromium",
+      '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary',
+      "#{ENV['HOME']}/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary",
+      '/Applications/Google Chrome Beta.app/Contents/MacOS/Google Chrome Beta',
+      "#{ENV['HOME']}/Applications/Google Chrome Beta.app/Contents/MacOS/Google Chrome Beta",
+    ]
+
+    potential_binaries.map do |bin|
+      path = `which #{bin}`.strip
+      Pathname.new(path).realpath.to_s if path.present?
+    end.concat(potential_paths.select do |path|
+      Pathname.new(path).realpath.to_s if File.exist?(path)
+    end).compact.first
+  end
+
   Capybara.register_driver :headless_chrome do |app|
-    chrome_options = { 'args' => ['headless', 'disable-gpu', 'no-sandbox'] }
-
-    if chrome_bin = ENV['CHROME_BIN']
-      chrome_options.merge!('binary' => chrome_bin)
-    end
-
     caps = Selenium::WebDriver::Remote::Capabilities.chrome(
-      "chromeOptions" => chrome_options
+      'chromeOptions' => {
+        'args' => ['headless', 'disable-gpu', 'no-sandbox'],
+        'binary' => ENV['CHROME_BIN'] || find_chrome_bin
+      }
     )
 
-    driver = Capybara::Selenium::Driver.new(
+    Capybara::Selenium::Driver.new(
       app,
       browser: :chrome,
       desired_capabilities: caps

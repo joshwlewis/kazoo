a025c07bc05e5a32c1c7ddeb65adafd1df71d7ca
Josh W Lewis <josh.w.lewis@gmail.com>
2017-04-21 16:47:56 -0500

    Add another system test

--- test/system/mascots_test.rb
+++ test/system/mascots_test.rb
@@ -6,4 +6,15 @@ class MascotsTest < ApplicationSystemTestCase
 
     assert_selector "h1", text: "Mascot"
   end
+
+  test "creating a mascot" do
+    visit new_mascot_url
+
+    fill_in "mascot[classification]", with: "goat"
+    fill_in "mascot[name]", with: "cloudy"
+
+    click_on "Create Mascot"
+
+    assert_selector "#notice", text: "Mascot was successfully created."
+  end
 end

6505d8e53455f8807d07ab163e8138c8ad2c2660
Andrew Appleton <andysapple@gmail.com>
2017-05-30 11:39:56 +0100

    Default Chrome path or GOOGLE_CHROME_BIN

--- config/initializers/browser.rb
+++ config/initializers/browser.rb
@@ -1,40 +1,10 @@
 if Rails.env.test?
-  def find_chrome_bin
-    potential_binaries = [
-      'chromium-browser',
-      'chromium',
-      'google-chrome-canary',
-      'google-chrome-unstable',
-      'google-chrome-beta',
-      'google-chrome-stable',
-      'google-chrome'
-    ]
-
-    potential_paths = [
-      '/Applications/Chromium.app/Contents/MacOS/Chromium',
-      "#{ENV['HOME']}/Applications/Chromium.app/Contents/MacOS/Chromium",
-      '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary',
-      "#{ENV['HOME']}/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary",
-      '/Applications/Google Chrome Beta.app/Contents/MacOS/Google Chrome Beta',
-      "#{ENV['HOME']}/Applications/Google Chrome Beta.app/Contents/MacOS/Google Chrome Beta",
-    ]
-
-    potential_binaries.map do |bin|
-      path = `which #{bin}`.strip
-      Pathname.new(path).realpath.to_s if path.present?
-    end.concat(potential_paths.select do |path|
-      Pathname.new(path).realpath.to_s if File.exist?(path)
-    end).compact.first
-  end
-
   Capybara.register_driver :headless_chrome do |app|
-    path = ENV['CHROME_BIN'] || find_chrome_bin
-    puts "Using Google Chrome at #{path}"
     caps = Selenium::WebDriver::Remote::Capabilities.chrome(
       'chromeOptions' => {
         'args' => ['headless', 'disable-gpu', 'no-sandbox'],
-        'binary' => path
-      }
+        'binary' => ENV['GOOGLE_CHROME_BIN']
+      }.reject { |k, v| v.nil? }
     )
 
     Capybara::Selenium::Driver.new(
